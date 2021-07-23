02b5f7cf3b77ebf8f2009d6af13e1ab4c14409af
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-07-23 12:16:25 +0200

    add global styles & main theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { ThemeProvider } from 'styled-components';
+import mainTheme from './styles/themes';
+import GlobalStyle from './styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle>
+      <ThemeProvider theme={mainTheme}>
+        <App />
+      </ThemeProvider>
+    </GlobalStyle>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/styles/themes/index.js
@@ -0,0 +1,25 @@
+const mainTheme = {
+  colors: {
+    white: '#FAFAFA',
+    black: '#212529',
+    blue: '#185ADB',
+    yellow: '#FFC947',
+    grey: '#EFEFEF',
+  },
+
+  fontSizes: {
+    xxl: '6.3rem',
+    xl: '3.3rem',
+    l: '1.4rem',
+    s: '1.3rem',
+    xs: '0.9rem',
+  },
+
+  fontWeights: {
+    normal: 400,
+    semiBold: 600,
+    bold: 700,
+  },
+};
+
+export default mainTheme;
