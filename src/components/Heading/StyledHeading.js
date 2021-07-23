b5f8d8ad5becce26ddaf0caa0456c6db49331068
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-07-23 17:52:54 +0200

    add headiing component

---
+++ src/components/Heading/StyledHeading.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StyledHeading = styled.h1`
+  font-size: ${({ theme }) => theme.fontSizes.xxl};
+  color: ${({ theme }) => theme.colors.black};
+`;
